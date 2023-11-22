05c38ba9a4c65697e88d9aad1ca9b8a62ca3d99b
Jabari <jabari.r.washington@gmail.com>
2023-11-21 20:13:14 -0600

    initial commit

---
+++ lookup/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html', {})#{} passes in python dictionary
+
+def about(request):
+    return render(request, 'about.html', {})#{} passes in python dictionary
